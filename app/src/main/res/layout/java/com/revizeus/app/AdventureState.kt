8c4c3712f71283370e80883963b1e9934b9669f9
Revizeus <mr.perez.thomas@gmail.com>
2026-04-23 01:01:07 +0200

    Delete app/src/main/res/layout/java directory

--- app/src/main/res/layout/java/com/revizeus/app/AdventureState.kt
+++
@@ -1,8 +0,0 @@
-package com.revizeus.app
-
-data class AdventureState(
-    val currentGodId: String? = null,
-    val currentSubject: String? = null,
-    val currentTempleLevel: Int = 0,
-    val currentMapIndex: Int = 0
-)
--- app/src/main/res/layout/java/com/revizeus/app/WorldMapState.kt
+++
@@ -1,7 +0,0 @@
-package com.revizeus.app
-
-data class WorldMapState(
-    val totalRestorationScore: Int,
-    val worldTier: Int,
-    val slots: List<WorldMapTempleSlot>
-)
